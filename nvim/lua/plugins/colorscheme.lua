6c17bdea2ca24ef943eda60acfd239c0958930d8
静叶知秋 <80163187+Luo-y3@users.noreply.github.com>
2025-10-09 22:02:14 +0700

    Update colorscheme.lua
    Change cursor theme to static

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -2,7 +2,7 @@ return {
   {
     "Shatur/neovim-ayu",
     lazy = false,
-    priority = 1000,
+    priority = 1001,
     config = function()
       require("ayu").setup({
         mirage = false,
@@ -18,7 +18,7 @@ return {
           Folded = { bg = "None" },
           FoldColumn = { bg = "None" },
           CursorLine = { bg = "None" },
-          CursorLineNr = { bg = "None" },
+          --CursorLineNr = { bg = "None" },
           CursorColumn = { bg = "None" },
           EndOfBuffer = { bg = "None" },
           ColorColumn = { bg = "None" },
@@ -38,6 +38,19 @@ return {
       })
 
       vim.cmd.colorscheme("ayu")
+
+      -- Make Static Cursor color
+      vim.cmd([[
+      highlight Cursor guifg=NONE guibg=#d2d4d6
+      ]])
+
+      -- Make Cursor shapes
+      vim.opt.guicursor = table.concat({
+        "n-v-c:block-Cursor", -- Normal mode change to Block style
+        "i-ci-ve:ver25-Cursor", -- Insert mode change to | style
+        "r-cr:hor20-Cursor", -- Replace Horizontal line
+        "o:hor50-Cursor", -- Operator pending Horizontal line
+      }, ",")
     end,
   },
 }
