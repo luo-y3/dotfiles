37fc960cd2a05a0349efc14b865e32116badc3b3
静叶知秋 <80163187+Luo-y3@users.noreply.github.com>
2025-10-09 18:10:58 +0700

    Add files via upload

---
+++ keymaps.lua
@@ -0,0 +1,23 @@
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+
+-- Split window
+keymap.set("n", "ss", ":split<Return>", opts)
+keymap.set("n", "sv", ":vsplit<Return>", opts)
+
+-- Move window
+keymap.set("n", "sh", "<C-w>h")
+keymap.set("n", "sk", "<C-w>k")
+keymap.set("n", "sj", "<C-w>j")
+keymap.set("n", "sl", "<C-w>l")
+
+-- Select all
+keymap.set("n", "<C-a>", "gg<S-v>G")
+
+-- Toggle Snacks Explorer
+keymap.set("n", "<Tab>", function()
+  require("snacks").explorer()
+end, opts)
+
+-- Coppy to Clipboard
+vim.opt.clipboard = "unnamedplus"
