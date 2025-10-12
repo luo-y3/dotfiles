3ef5b3a995da6c709a8964543aeea6c2a321e0e7
静叶知秋 <80163187+Luo-y3@users.noreply.github.com>
2025-10-12 17:27:32 +0700

    Update ui.lua
    make lualine look better

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -103,29 +103,19 @@ return {
           section_separators = { left = "", right = "" },
         },
         sections = {
-          -- lualine_a = { { "mode", separator = { left = "" }, right_padding = 2 } },
-          lualine_b = {
-            {
-              "filename",
-              path = 1,
-            },
-            "branch",
-          },
-          lualine_c = {
-            "%=",
-          },
+          lualine_a = { "mode" },
+          lualine_b = { "branch", "diagnostics" },
+          lualine_c = { { "filename", path = 1 } },
           lualine_x = {},
           lualine_y = { "filetype", "progress" },
-          lualine_z = {
-            -- { "location", separator = { right = "" }, left_padding = 2 },
-          },
+          lualine_z = { "location" },
         },
         inactive_sections = {
           lualine_a = { "filename" },
-          lualine_b = {},
-          lualine_c = {},
+          lualine_b = { "branch", "diagnostics" },
+          lualine_c = { { "filename", path = 1 } },
           lualine_x = {},
-          lualine_y = {},
+          lualine_y = { "filetype", "progress" },
           lualine_z = { "location" },
         },
         tabline = {},
