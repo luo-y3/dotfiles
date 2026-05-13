6aabc94e0d4128314fa08db2633bfa79a2ca6f8d
静叶知秋 <80163187+luo-y3@users.noreply.github.com>
2026-05-13 22:45:03 +0700

    Add files via upload

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,13 +1,22 @@
 return {
   {
     "luo-y3/zen-sight.nvim",
-    lazy = false,
-    priority = 1000,
     config = function()
       require("zen-sight").setup({
-        tranparent = true,
+        transparent = true,
+        -- theme = "zen_night",
+        -- theme = "zen_meadow",
       })
       vim.cmd.colorscheme("zen-sight")
     end,
+    -- "craftzdog/solarized-osaka.nvim",
+    -- lazy = false,
+    -- priority = 1000,
+    -- config = function()
+    --   require("solarized-osaka").setup({
+    --     transparent = true,
+    --   })
+    --   vim.cmd.colorscheme("solarized-osaka")
+    -- end,
   },
 }
--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -127,6 +127,20 @@ return {
 ███████╗██║  ██║███████╗██║ ╚████║╚██████╔╝██║ ╚████║██║██║  ██║██║ ╚████║
 ╚══════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═══╝ ╚═════╝ ╚═╝  ╚═══╝╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝
    ]],
+          keys = {
+            { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+            { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+            { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+            {
+              icon = " ",
+              key = "c",
+              desc = "Config",
+              action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
+            },
+            { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+            { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
+            { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+          },
         },
       },
     },
